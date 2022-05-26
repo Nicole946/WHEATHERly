f96499dc00cd39fe832eec8c3519028128830b92
Nicole <nika.havalcova@gmail.com>
2022-05-26 18:14:23 +0100

    Fixed forecast days order

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ todayDay.innerHTML = `${day}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   return days[day];
 }
